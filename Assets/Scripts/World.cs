66da177bab9ebaa1b7019b580f8916a22d899b99
iyoka <kato-hiroto@ed.tmu.ac.jp>
2017-08-30 13:27:38 +0900

    現状

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class World {
+
+
+
+
+	public static void MinoReset(){
+
+	}
+
+	public static void GameReset(){
+		
+	}
+
+}
