a5f308602d67ac67b6195035555bd68b11954120
unknown <>
2017-08-31 01:05:46 +0900

    タイトル→セッティング遷移、ゲーム終了処理 セッティングボタン配置

--- Assets/Scripts/StartScene.cs
+++ Assets/Scripts/StartScene.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartScene : MonoBehaviour {
 
+	Button Title;
+	Button Option;
+	Button Exit;
+
 	// Use this for initialization
 	void Start () {
-		
+		// ボタンコンポーネントの取得
+		Title   = GameObject.Find ("/Canvas/TitleButton").GetComponent<Button> ();
+		Option  = GameObject.Find ("/Canvas/OptionButton").GetComponent<Button> ();
+		Exit    = GameObject.Find ("/Canvas/ExitButton").GetComponent<Button> ();
+
+		//最初に選択状態するボタン
+		Title.Select ();
 	}
 	
 	// Update is called once per frame
